841bc55e72f3d04325332cf6c2acaabef8ea0154
thehstatus-art <thehstatus@gmail.com>
2026-02-26 21:11:21 -0500

    Fix missing useLocation import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header({ cart, setCheckoutOpen }) {
   const location = useLocation();

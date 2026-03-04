7e496851e411913b74b7b8cdf34bf8f63280b240
thehstatus-art <thehstatus@gmail.com>
2026-03-03 22:30:44 -0500

    Fix duplicate React imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 
 
 function Home({ products, addToCart }) {
